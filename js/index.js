b9b11e5fdbe0e0e97d87f28bd0363a54d5a0f7d4
yangguangyuan <yangguangyuan@hzboc.com>
2016-09-11 15:47:30 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-require(['jquery','vue','public','less'],function ($,vue,public) {
-    
+require(['jquery','vue','less'],function ($,vue) {
+
     $.getJSON('./js/index.json').done(function (rs) {
 
         new vue({
             el: '#index',
             data: rs,
             ready:function () {
-                public._back()
+
             }
         })
 
